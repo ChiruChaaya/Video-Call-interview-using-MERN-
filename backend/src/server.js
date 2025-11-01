f6cf995092f927b319c7be33e1e5369444df2414
ChiruChaaya0212 <chirum01004@gmail.com>
2025-11-01 20:34:27 +0530

    changing 01

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,11 @@ import {Env} from "./lib/env.js";
 
 const app = express();
 
-console.log(process.env.PORT);
+console.log(Env.PORT);
 app.get("/", (req, res) => {
     res.status(200).json({msg:"Server is running succesufully"});
 });
 
 app.listen(Env.PORT, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port",Env.PORT);
 });
