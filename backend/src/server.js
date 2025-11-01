5a77c5444ae3da302a3ca44ba16c6dc190cc4cc9
ChiruChaaya0212 <chirum01004@gmail.com>
2025-11-01 20:16:42 +0530

    Backend env setting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+console.log(process.env.PORT);
 app.get("/", (req, res) => {
     res.status(200).json({msg:"Server is running succesufully"});
 });
@@ -8,4 +12,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
-
